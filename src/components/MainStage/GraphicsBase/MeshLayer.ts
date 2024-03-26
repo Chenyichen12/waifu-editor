2f128e153bdccd41ba3afff9f50fb17248cd6034
chenyichen <sama1538@outlook.com>
2024-03-26 15:18:13 +0800

    让Mesh复杂一些

--- src/components/MainStage/GraphicsBase/MeshLayer.ts
+++ src/components/MainStage/GraphicsBase/MeshLayer.ts
@@ -54,21 +54,28 @@ class MeshLayer extends Graphics {
         const topRight = new MeshPoint(left + width, top, 1, 0, this);
         const buttonLeft = new MeshPoint(left, top + height, 0, 1, this);
         const buttonRight = new MeshPoint(left + width, top + height, 1, 1, this);
+
+        const topMiddle = new MeshPoint(left + width / 2, top, 0.5, 0, this);
+        const buttonMiddle = new MeshPoint(left + width / 2, top + height, 0.5, 1, this);
+
         this.pointList.push(topLeft);
         this.pointList.push(topRight);
         this.pointList.push(buttonLeft);
         this.pointList.push(buttonRight);
+        this.pointList.push(topMiddle);
+        this.pointList.push(buttonMiddle);
 
-        const line1 = new MeshLine(topLeft, topRight);
-        const line2 = new MeshLine(topLeft, buttonLeft);
+        const line1 = new MeshLine(topLeft, topMiddle);
+        const line2 = new MeshLine(topMiddle, topRight);
         const line3 = new MeshLine(topRight, buttonRight);
-        const line4 = new MeshLine(buttonLeft, buttonRight);
-        const line5 = new MeshLine(topLeft, buttonRight);
-        this.lineList.push(line1);
-        this.lineList.push(line2);
-        this.lineList.push(line3);
-        this.lineList.push(line4);
-        this.lineList.push(line5);
+        const line4 = new MeshLine(buttonRight, buttonMiddle);
+        const line5 = new MeshLine(buttonMiddle, buttonLeft);
+        const line6 = new MeshLine(buttonLeft, topLeft);
+        const line7 = new MeshLine(topLeft, buttonMiddle);
+        const line8 = new MeshLine(topMiddle, buttonRight);
+        const line9 = new MeshLine(topMiddle, buttonMiddle);
+
+        this.lineList.push(line1, line2, line3, line4, line5, line6, line7, line8, line9);
     }
 
 }
