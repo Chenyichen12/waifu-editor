7f7192135576ba970e129f42e5863beacb03512c
chenyichen <sama1538@outlook.com>
2024-03-27 16:37:27 +0800

    实现点击选中 不过很屎山

--- src/components/MainStage/StageApp.ts
+++ src/components/MainStage/StageApp.ts
@@ -1,8 +1,9 @@
-import { Application, ApplicationOptions, EventBoundary, Graphics } from "pixi.js";
-import { ref, shallowRef } from "vue";
+import { Application, ApplicationOptions, DestroyOptions, EventBoundary, Graphics, Matrix, RendererDestroyOptions } from "pixi.js";
+import { ref, shallowRef, watch } from "vue";
 import Project from "../Project/Project";
 import GraphicsLayer from "./GraphicsLayer";
 import { Group, LayerType, NormalLayer, Root } from "../Project/LayerStruct";
+import MeshLayer from "./GraphicsBase/MeshLayer";
 
 
 const instanceApp = shallowRef<StageApp | null>(null)
@@ -14,11 +15,31 @@ class StageApp extends Application {
     stageDom
     mouseState: StageState = new StageNormalState
     graphicsChildren: GraphicsLayer[] = []
+
+    selectedMesh
+    unWatchSelected
     constructor(dom: HTMLDivElement) {
         super();
         this.stageDom = dom;
         this.stage.interactive = true;
+        this.selectedMesh = shallowRef<GraphicsLayer[]>([]);
+        this.unWatchSelected = watch(this.selectedMesh, (newV, oldV) => {
+            oldV.forEach((v) => {
+                v.showMesh = false;
+            })
+            newV.forEach((v, i) => {
+                v.showMesh = true;
+                v.mesh.setFromMatrix(v.relativeGroupTransform);
+                this.stage.addChild(v.mesh);
+                v.mesh.zIndex = this.graphicsChildren.length + 1 + i;
+            })
+        })
         instanceApp.value = this;
+    }
+
+    destroy(rendererDestroyOptions?: RendererDestroyOptions | undefined, options?: DestroyOptions | undefined): void {
+        this.unWatchSelected();
+        super.destroy(rendererDestroyOptions, options);
     }
     async init(options?: (Partial<ApplicationOptions> | undefined)): Promise<void> {
         if (Project.instance.value?.root == null) {
@@ -72,8 +93,8 @@ class StageApp extends Application {
     protected addSprite() {
         const proRoot = Project.instance.value!.root;
         this.addLayer(proRoot);
-        this.graphicsChildren.reverse();
-        this.graphicsChildren.forEach((v) => {
+        this.graphicsChildren.forEach((v, i) => {
+            v.zIndex = this.graphicsChildren.length - i;
             this.stage.addChild(v);
         })
     }
@@ -84,7 +105,8 @@ class StageApp extends Application {
                 const item = Project.instance.value!.assetList.get(normal.assetId);
                 if (item == null) continue;
                 const gra = new GraphicsLayer({ texture: item });
-                gra.position.set(item.bound.left, item.bound.top);
+                const tranformMat = new Matrix(1, 0, 0, 1, item.bound.left, item.bound.top);
+                gra.setFromMatrix(tranformMat);
                 this.graphicsChildren.push(gra);
             } else {
                 const gro = child as Group;
@@ -121,10 +143,11 @@ class StageNormalState extends StageState {
         const stagePos = context.stage.toLocal({ x: e.offsetX, y: e.offsetY });
         for (const gra of context.graphicsChildren) {
             if (gra.containsPoint(stagePos)) {
-                gra.showMesh = true;
-                break;
+                context.selectedMesh.value = [gra];
+                return
             }
         }
+        context.selectedMesh.value = [];
     }
     onMouseMove(_e: MouseEvent, _context: StageApp): void {
         return;
