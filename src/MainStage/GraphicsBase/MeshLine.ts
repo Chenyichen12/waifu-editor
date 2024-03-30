ce8bc7f69a067c819c30efef4b20fdf821d714f6
Chenyichen12 <sama1538@outlook.com>
2024-03-30 10:52:32 +0800

    修正MeshLine的判断hit的逻辑

--- src/MainStage/GraphicsBase/MeshLine.ts
+++ src/MainStage/GraphicsBase/MeshLine.ts
@@ -1,3 +1,7 @@
+/*
+ * @Author: Chenyichen12 sama1538@outlook.com
+ * @Date: 2024-03-28 12:43:31
+ */
 import MeshPoint from './MeshPoint'
 class MeshLine {
     p1: MeshPoint
@@ -25,6 +29,31 @@ class MeshLine {
         if (p === this.p2)
             return this.p1;
         return null
+    }
+
+    ifHitLine(x: number, y: number, hitDistance: number = 1): boolean {
+        const top = this.p1.y > this.p2.y ? this.p2.y : this.p1.y;
+        const button = this.p1.y > this.p2.y ? this.p1.y : this.p2.y;
+        const left = this.p1.x > this.p2.x ? this.p2.x : this.p1.x;
+        const right = this.p1.x > this.p2.x ? this.p1.x : this.p2.x;
+
+        if (x > right + hitDistance || x < left - hitDistance || y > button + hitDistance || y < top - hitDistance)
+            return false;
+        return hitDistance >= MeshLine.distanceFromLine(x, y, this);
+    }
+    /**
+     * 点距离线的距离
+     * @param x 
+     * @param y 
+     * @param line 
+     * @returns 
+     */
+    static distanceFromLine(x: number, y: number, line: MeshLine): number {
+        const A = line.p1.y - line.p2.y;
+        const B = line.p2.x - line.p1.x;
+        const C = line.p1.x * line.p2.y - line.p2.x * line.p1.y
+        const f = A * x + B * y + C;
+        return (f * f) / (A * A + B * B);
     }
 }
 export default MeshLine
