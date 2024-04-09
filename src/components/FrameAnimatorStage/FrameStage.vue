098dc430856e4657cd06635bfea58eb4bc21a101
ShenXudong666 <2273679544@qq.com>
2024-04-09 23:22:47 +0800

    尝试滑块组件和滑动的效果

--- src/components/FrameAnimatorStage/FrameStage.vue
+++ src/components/FrameAnimatorStage/FrameStage.vue
@@ -1,48 +1,131 @@
 
 
 <script setup lang="ts">
-import { ElContainer, ElHeader, ElMain, ElScrollbar, ElButton, ElFooter } from 'element-plus';
+import { ElContainer, ElHeader, ElButton, ElFooter } from 'element-plus';
+import { Plus,Delete} from '@element-plus/icons-vue';
 import { onMounted } from "vue";
+import { ref } from "vue";
 
+//  为样例滑块添加数值
+const value = ref(0)
+const value1=ref(0)
+const value2=ref(0)
+const value3=ref(0)
+const value4=ref(0)
+const value5=ref(0)
 
 onMounted(() => {
     
 })
 
+const formatTooltip = (val: number) => {
+  return val / 100
+}
+
+
 </script>
 
 <template>
-<el-container>
-    <el-header>
-      <el-button type="primary" icon="el-icon-plus" size="small">添加关键帧</el-button>
-      <el-button icon="el-icon-delete" size="small">移除关键帧</el-button>
-    </el-header>
-    <el-main>
-      <el-scrollbar>
-        <ul>
-          <!-- <li v-for="entry in entryList" :key="entry.name">
-            <span>{{ entry.name }}</span>
-            <div class="keyframes">
-              <span v-for="(value, index) in entry.keyframes" :key="index">
-                {{ value }}
-              </span>
-            </div>
-            <el-button type="danger" icon="el-icon-delete">删除</el-button>
-          </li> -->
-        </ul>
-      </el-scrollbar>
-    </el-main>
-    <el-footer>
+
+    
+      <el-button type="primary"  :icon="Plus"  circle size="small" class="headbtn"></el-button>
+      
+      <el-button type="success" :icon='Plus' circle size="small" class="headbtn"></el-button>
+    
+      <el-button type="warning" :icon="Delete" circle size="small" class="headbtn"></el-button>
+    
+      <div><el-scrollbar height="570px" id="scrollbar">
+          <div class="slider-demo-block">
+      <!-- 测试组件效果先把名字写死，后续再改 -->
+     
+    <span class="demonstration" >眉毛1</span>
+    <el-slider v-model="value" :format-tooltip="formatTooltip"  size="small"/>
+  </div>
+  <div class="slider-demo-block">
+      <!-- 测试组件效果先把名字写死，后续再改 -->
+     
+    <span class="demonstration" >眉毛1</span>
+    <el-slider v-model="value1" :format-tooltip="formatTooltip"  size="small"/>
+  </div>
+
+  <div class="slider-demo-block">
+      <!-- 测试组件效果先把名字写死，后续再改 -->
+     
+    <span class="demonstration" >眉毛1</span>
+    <el-slider v-model="value2" :format-tooltip="formatTooltip"  size="small"/>
+  </div>
+  
+  </el-scrollbar></div>
+  
+  <el-button type="primary"  :icon="Plus"  circle size="small" class="footbtn" style="float: right;"></el-button>
+
+  <el-button type="primary"  :icon="Plus"  circle size="small" class="footbtn" style="float: right;"></el-button>
+
+        
+      
+      
+      
+    
       <!-- <el-input v-model="newEntryName" placeholder="请输入新条目名称" /> -->
-      <el-button type="primary" icon="el-icon-plus">添加条目</el-button>
-    </el-footer>
-  </el-container>
+      <!-- <el-button type="primary" icon="el-icon-plus"  circle class="btn"></el-button> -->
+    
 
 
 </template>
 
 <style>
 
+/* 滑动的样式，进一步需要迁移到另外的css文件 */
+.slider-demo-block {
+  max-width: 180px;
+  display: flex;
+  align-items: center;
+  
+}
+.slider-demo-block  {
+  margin-top: 0;
+  margin-left: 0;
+  margin-right: 0;
+  margin-bottom: 0;
+}
+
+.slider-demo-block .demonstration {
+  font-size: 14px;
+  color: var(--el-text-color-secondary);
+  /* line-height: 44px; */
+  flex: 1;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+  margin-bottom: 0;
+  margin-top: 0;
+  margin-right:0;
+  margin-left:0;
+}
+.slider-demo-block .demonstration + .el-slider {
+  flex: 0 0 70%;
+}
+
+/* //顶部和尾部按钮的原因是靠左和靠右的间距 */
+.headbtn {
+  margin-right: 0px;
+  margin-left:0px;
+}
+.footbtn {
+  margin-right: 5px;
+  margin-left:0px;
+}
+
+#scrollbar {
+  width: 180px;
+}
+  
+.list
+{
+  height: 400px;
+}
+
 
 
 </style>
+
