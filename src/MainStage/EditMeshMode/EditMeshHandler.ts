114882146d2251eeeb7fc90dbc92c190a24a79d7
Chenyichen12 <sama1538@outlook.com>
2024-04-09 16:06:00 +0800

    完成编辑拖拽的实现

--- src/MainStage/EditMeshMode/EditMeshHandler.ts
+++ src/MainStage/EditMeshMode/EditMeshHandler.ts
@@ -2,8 +2,10 @@
  * @Author: Chenyichen12 sama1538@outlook.com
  * @Date: 2024-04-08 07:53:09
  */
+import { da } from "element-plus/es/locales.mjs";
 import StageEventHandler, { DragStageEventHandler, RectSelectEventHandler, SelectedEventHandler, StageEventRes } from "../EventHandler/StageEventHandler";
 import MeshPoint from "../GraphicsBase/MeshPoint";
+import RectInSelected from "../GraphicsBase/RectInSelected";
 import StageApp from "../StageApp";
 import { xy } from "../TwoDType";
 import EditMeshMode from "./EditMeshMode";
@@ -60,7 +62,20 @@ class EditHandler extends SelectedEventHandler {
             this.changeToState(new DragPointHandler(this.context, this.mode, hitPoint));
             return StageEventRes.DEFAULT;
         }
+        if (RectInSelected.ifHitRect(this.mode.editMesh.selectedPoints, point)) {
+            this.changeToState(new RectDragHandler(this.context, this.mode, this.mode.editMesh.selectedPoints, point));
+            return StageEventRes.DEFAULT;
+        }
         this.changeToState(new RectSelect(this.toStagePos(e.offsetX, e.offsetY), this.context, this.mode));
+        return StageEventRes.DEFAULT
+    }
+    handleMouseMoveEvent(e: MouseEvent): StageEventRes {
+        const point = this.mode.targetLayer.transformFormStage(this.toStagePos(e.offsetX, e.offsetY));
+        let hitRect = false
+        if (RectInSelected.ifHitRect(this.mode.editMesh.selectedPoints, point)) {
+            hitRect = true;
+        }
+        this.context.containerDom.style.cursor = hitRect ? "move" : "default";
         return StageEventRes.DEFAULT
     }
 
@@ -128,5 +143,38 @@ class RectSelect extends RectSelectEventHandler {
         this.changeToState(new EditHandler(this.context, this.mode));
         return StageEventRes.DEFAULT;
     }
+}
+
+class RectDragHandler extends StageEventHandler {
+    protected mode: EditMeshMode
+    protected lastMove: xy
+
+    protected firstMovePoint: xy
+    selectPoints
+    constructor(context: StageApp, mode: EditMeshMode, selectPoints: MeshPoint[], firMove: xy) {
+        super(context);
+        this.mode = mode;
+        this.selectPoints = selectPoints;
+        this.firstMovePoint = firMove;
+        this.lastMove = firMove;
+    }
+
+    handleMouseMoveEvent(e: MouseEvent): StageEventRes {
+
+        const point = this.mode.targetLayer.transformFormStage(this.toStagePos(e.offsetX, e.offsetY));
+        RectInSelected.dragRectPoint(this.selectPoints, point.x - this.lastMove.x, point.y - this.lastMove.y, (_p) => {
+            const delaunay = new Delaunay<MeshPoint>(this.mode.editMesh.listPoint);
+            const data = delaunay.getTriangleData();
+            this.mode.editMesh.setPoint(data.vertices, data.triangles);
+            this.mode.editMesh.upDate();
+        })
+        this.lastMove = point;
+        return StageEventRes.DEFAULT;
+    }
+    handleMouseUpEvent(_e: MouseEvent): StageEventRes {
+        this.changeToState(new EditHandler(this.context, this.mode));
+        return StageEventRes.DEFAULT;
+    }
+
 }
 export default EditHandler;
