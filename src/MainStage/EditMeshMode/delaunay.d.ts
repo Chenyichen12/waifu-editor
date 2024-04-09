6fe333fd37985fc60edd0f388297349ec4aa4cd2
Chenyichen12 <sama1538@outlook.com>
2024-04-09 12:13:05 +0800

    引入外部三角化算法

---
+++ src/MainStage/EditMeshMode/delaunay.d.ts
@@ -0,0 +1,13 @@
+/*
+
+ * @Author: Chenyichen12 sama1538@outlook.com
+ * @Date: 2024-04-09 11:12:54
+ */
+import { xy } from "../TwoDType.js";
+import "./delaunay.js"
+
+class Delaunay {
+    getTriangleData(): { vertices: xy[], triangles: number[] };
+    constructor(point: xy[]);
+}
+export default Delaunay
--- src/MainStage/EditMeshMode/testDelaunay.ts
+++ src/MainStage/EditMeshMode/testDelaunay.ts
@@ -2,17 +2,17 @@
  * @Author: Chenyichen12 sama1538@outlook.com
  * @Date: 2024-04-08 21:18:12
  */
-import MeshPoint from "../GraphicsBase/MeshPoint";
-import Delaunay from "./Delaunay";
 
-export default function test() {
-    const p1 = new MeshPoint(10, 2, 0, 0);
+import Delaunay from "./delaunay"
 
-    const p2 = new MeshPoint(0, 5, 10, 2);
-
-    const p3 = new MeshPoint(9, 8, 0, 1);
-    const p4 = new MeshPoint(5, 3, 1, 2);
-    const ans = Delaunay([p1, p2, p3, p4]);
-    console.log(ans);
-
+function test() {
+    const testPoints = [
+        { x: 8000, y: 2000 },
+        { x: 1000, y: 3000 },
+        { x: 20, y: 20 },
+        { x: 10, y: 80 }
+    ];
+    const delaunay = new Delaunay(testPoints);
+    console.log(delaunay.getTriangleData());
 }
+export default test;
